62216ebe7023e27eaaaed61920232e0fdaa9e2e6
Yunzhe Yu <yuyz2021@bu.edu>
2024-01-30 12:04:28 -0500

    8/10 trying to fix

--- assigns/assign01/assign01_04.ml
+++ assigns/assign01/assign01_04.ml
@@ -17,15 +17,15 @@
  *)
 
 let taxicab (n : int) : int =
-    let max_val = int_of_float (float_of_int n ** (1. /. 3.)) in
-    let rec find_pairs x y count =
-      if x > max_val then count
-      else if y > max_val then find_pairs (x + 1) (x + 1) count
-      else 
-        let cube_sum = x * x * x + y * y * y in
-        if cube_sum > n then find_pairs x (y + 1) count
-        else if cube_sum < n then find_pairs (x + 1) y count
-        else find_pairs (x + 1) (y + 1) (count + 1)
-    in
-    find_pairs 1 1 0
+  let max_val = int_of_float (float_of_int n ** (1. /. 3.)) in
+  let rec find_pairs x y count =
+    if x > max_val then count
+    else if y > max_val then find_pairs (x + 1) (x + 1) count
+    else 
+      let cube_sum = x * x * x + y * y * y in
+      if cube_sum = n then find_pairs x (y + 1) (count + 1)
+      else if cube_sum > n then find_pairs (x + 1) x count
+      else find_pairs x (y + 1) count
+  in
+  find_pairs 1 1 0
   
