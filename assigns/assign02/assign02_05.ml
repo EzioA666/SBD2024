4710e9b9ea7bfbe4ecead5fccdfce9f78e7f4500
Yunzhe Yu <yuyz2021@bu.edu>
2024-02-06 12:05:01 -0500

    02_03 fixed

--- assigns/assign02/assign02_05.ml
+++ assigns/assign02/assign02_05.ml
@@ -47,42 +47,5 @@ type point = {
 }
 
 let rec all_paths (len : int) (stp : point) (endp : point) : (dir * int) list list =
-  (* Helper function to move a point in a given direction *)
-  let move (p : point) (d : dir) (dist : int) : point =
-    match d with
-    | N -> {p with y = p.y + dist}
-    | S -> {p with y = p.y - dist}
-    | E -> {p with x = p.x + dist}
-    | W -> {p with x = p.x - dist}
-  in
-
-  (* Helper function to check if a path ends at the end point *)
-  let rec path_ends_at (p : point) (path : (dir * int) list) : bool =
-    match path with
-    | [] -> p = endp
-    | (d, dist) :: rest -> path_ends_at (move p d dist) rest
-  in
-
-  (* Generate all possible sequences of moves *)
-  let rec generate_paths (len : int) (current_path : (dir * int) list) : (dir * int) list list =
-    if len = 0 then
-      if path_ends_at stp current_path then [List.rev current_path] else []
-    else
-      let dirs = [N; S; E; W] in
-      List.fold_left (fun acc d ->
-        if len = 1 || (current_path <> [] && fst (List.hd current_path) = d) then acc
-        else
-          List.append acc (generate_paths (len - 1) ((d, 1) :: current_path))
-      ) [] dirs
-  in
-
-  (* Function to merge consecutive steps in the same direction *)
-  let rec merge_consecutive (path : (dir * int) list) : (dir * int) list =
-    match path with
-    | (d1, n1) :: (d2, n2) :: rest when d1 = d2 -> merge_consecutive ((d1, n1 + n2) :: rest)
-    | head :: rest -> head :: merge_consecutive rest
-    | [] -> []
-  in
-
-  let raw_paths = generate_paths len [] in
-  List.map merge_consecutive raw_paths
+  assert false
+  (*TODO*)
