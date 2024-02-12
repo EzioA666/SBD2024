b60ba2b02aba7e8bc4e4a214f56d24e8d5b96389
Yunzhe Yu <yuyz2021@bu.edu>
2024-02-12 17:44:04 -0500

    hw1 done

--- assigns/assign03/assign03_01.ml
+++ assigns/assign03/assign03_01.ml
@@ -26,4 +26,21 @@ type 'a concatlist
   | Concat of 'a concatlist * 'a concatlist
 
 let sort (l : 'a concatlist) : 'a list =
-  assert false (* TODO *)
+  let rec merge_sort (l : 'a concatlist) : 'a list =
+    match l with
+    | Nil -> []
+    | Single x -> [x]
+    | Concat (l1, l2) ->
+      let l1' = merge_sort l1 in
+      let l2' = merge_sort l2 in
+      merge l1' l2'
+  and merge (l1 : 'a list) (l2 : 'a list) : 'a list =
+    match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | h1 :: t1, h2 :: t2 ->
+      if h1 <= h2 then h1 :: merge t1 l2 else h2 :: merge l1 t2
+  in
+  merge_sort l
+
+  
