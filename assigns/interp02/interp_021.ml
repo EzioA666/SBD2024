5e858a16b75cbfbd1a3b0fadb028b62c9c7f8c1d
Yunzhe Yu <yuyz2021@bu.edu>
2024-04-24 18:10:00 -0400

    dogshit

--- assigns/interp02/interp_021.ml
+++ assigns/interp02/interp_021.ml
@@ -108,7 +108,7 @@ type const
 
 type command
   = Push of const | Trace
-  | Add | Mul | Div 
+  | Add | Mul | Div
   | And | Or | Not | Lt | Eq
   | If of program * program
   | While of program * program
@@ -131,7 +131,7 @@ and value
 type record =
   { id : int
   ; local : bindings
-  ; called_def_id : int
+  ; caller_def_id : int
   ; return_prog : program
   }
 
@@ -177,10 +177,6 @@ let parse_int =
 let parse_bool =
   (str "True" >| true) <|> (str "False" >| false)
 
-let parse_Bconst = parse_bool >|= fun x -> Bool x
-let parse_Nconst = parse_int >|= fun x -> Num x
-let parse_const = parse_Bconst <|> parse_Nconst
-
 let parse_comment =
   char '(' >> many (satisfy ((<>) ')')) >> char ')' >| ()
 
@@ -191,44 +187,7 @@ let ws = many (ws >> parse_comment) >> ws
 let keyword w = str w << ws
 
 let rec parse_com () =
-  let parse_fun =
-    let* _ = keyword ":" in
-    let* body = parse_prog_rec () in
-    let* _ = keyword ";" in
-    pure (Fun body)
-  in
-  let parse_arith =
-    choice
-    [ char '+' >| Add
-    ; char '*' >| Mul
-    ; char '/' >| Div
-    ; char '=' >| Eq
-    ; char '<' >| Lt
-    ]
-  in
-  let parse_logic =
-    choice
-    [ keyword "&&" >| And
-    ; keyword "||" >| Or
-    ; char '~' >| Not
-    ]
-  in
-  let parse_bind =
-    let* _ = keyword "|>" in
-    let* id = parse_ident in
-    pure (Bind id) 
-  in
-  let parse_fetch =
-    parse_ident >|= fun id -> Fetch id
-  in
-  let parse_trace =
-    keyword "." >| Trace
-  in
-  let parse_call =
-    keyword "#" >| Call
-  in
-  let parse_return =
-    keyword "Return" >| Return
+  let parse_fun = fail (* TODO *)
   in
   let parse_if =
     let* _ = keyword "?" in
@@ -249,14 +208,6 @@ let rec parse_com () =
   choice
     (* TODO: Add more alternatives *)
     [ parse_fun
-    ; parse_const >|= (fun s -> Push s)
-    ; parse_arith 
-    ; parse_logic 
-    ; parse_bind 
-    ; parse_fetch 
-    ; parse_trace 
-    ; parse_call 
-    ; parse_return 
     ; parse_while
     ; parse_if
     ; parse_ident >|= (fun s -> Fetch s)
@@ -270,46 +221,19 @@ let parse_prog = parse (ws >> parse_prog_rec ())
 (* FETCHING AND UPDATING *)
 
 (* fetch the value of `x` in the environment `e` *)
-let rec fetch_env e x =  (* TODO *)
-match e with
-| Global bindings -> List.assoc_opt x bindings 
-| Local (record, enclosing_env) -> 
-  (match List.assoc_opt x record.local with
-  | Some _ as result -> result 
-  | None -> fetch_env enclosing_env x) 
+let fetch_env e x = None (* TODO *)
 
-let rec update_env e x v = (* TODO *)
-match e with
-| Global bindings -> Global ((x, v) :: bindings) 
-| Local (record, enclosing_env) ->
-  if List.exists (fun (var, _) -> var = x) record.local then
-    let updated_locals = List.map (fun (var, value) -> if var = x then (x, v) else (var, value)) record.local in
-    Local ({record with local = updated_locals}, enclosing_env)
-  else
-    let updated_enclosing = update_env enclosing_env x v in
-    Local (record, updated_enclosing)
+let rec update_env e x v = Global [] (* TODO *)
 
 (* EVALUTION *)
 
-
 (* make the panic configuration given a configuration *)
-(*let panic (_, _, t, _) msg = [], Global [], ("panic: " ^ msg) :: t, []*)
-
-(* Panic function to handle errors *)
 let panic (_, _, t, _) msg = [], Global [], ("panic: " ^ msg) :: t, []
-
-(* Utility function to get the current scope identifier *)
-
-let local_id = function
-  | Global _ -> 0  (* Global scope has ID 0 *)
-  | Local (r, _) -> r.id  (* Local scope has specific ID *)
 
 let eval_step (c : stack * env * trace * program) =
   match c with
-  
   (* Push *)
-  | (s, e, t, Push (Num n) :: p) -> (Const (Num n) :: s, e, t, p)
-  | (s, e, t, Push (Bool b) :: p) -> (Const (Bool b) :: s, e, t, p)
+  | s, e, t, Push c :: p -> Const c :: s, e, t, p
   (* Trace *)
   | v :: s, e, t, Trace :: p -> s, e, to_string v :: t, p
   | [], _, _, Trace :: _ -> panic c "stack underflow (. on empty)"
@@ -318,66 +242,8 @@ let eval_step (c : stack * env * trace * program) =
   | _ :: _ :: _, _, _, Add :: _ -> panic c "type error (+ on non-integers)"
   | _ :: [], _, _, Add :: _ -> panic c "stack underflow (+ on single)"
   | [], _, _, Add :: _ -> panic c "stack underflow (+ on empty)"
-  (* TODO *)
-  (*Mul Div shit*)
-  | Const (Num m) :: Const (Num n) :: s, e, t, Mul :: p -> Const (Num (m * n)) :: s, e, t, p
-  | Const (Num m) :: Const (Num n) :: s, e, t, Div :: p when n != 0 -> Const (Num (m / n)) :: s, e, t, p
-  | Const (Num _) :: Const (Num 0) :: _, _, _, Div :: _ -> panic c "division by zero"
-  (*Logical Operations*)
-  | Const (Bool b1) :: Const (Bool b2) :: s, e, t, And :: p -> Const (Bool (b1 && b2)) :: s, e, t, p
-  | Const (Bool b1) :: Const (Bool b2) :: s, e, t, Or :: p -> Const (Bool (b1 || b2)) :: s, e, t, p
-  | Const (Bool b) :: s, e, t, Not :: p -> Const (Bool (not b)) :: s, e, t, p
-  (*Bind*)
-  | v :: s, e, t, Bind ident :: p -> 
-    let e' = update_env e ident v in
-    (s, e', t, p)
-  (*Fetch*)
-  | s, e, t, Fetch ident :: p ->
-    (match fetch_env e ident with
-     | Some v -> (v :: s, e, t, p)
-     | None -> panic (s, e, t, p) ("unbound identifier: " ^ ident))
-  (*If Else*)
-  | Const (Bool true) :: s, e, t, If (then_p, _) :: p -> s, e, t, then_p @ p
-  | Const (Bool false) :: s, e, t, If (_, else_p) :: p -> s, e, t, else_p @ p
-  | _ :: s, e, t, While (cond_p, body_p) :: p -> s, e, t, If (cond_p, body_p @ [While (cond_p, body_p)]) :: p
-  (*function defition*)
-  | s, e, t, Fun prog :: p -> 
-    let closure = Clos { def_id = local_id e; captured = []; prog } in
-    (closure :: s, e, t, p)
+  | _ -> assert false (* TODO *)
 
-  | Clos {def_id;captured;prog}::s, e, t, Call::p -> 
-      let loc_id = local_id e +1 in 
-      let new_rec = {id = def_id;local=captured;called_def_id = loc_id ;return_prog = prog} in 
-      s, Local(new_rec,e), t, prog@p 
-
-  | _, _, _, Call :: _ -> panic c "Call command without closure on the stack"
-  (*Debug*)
-    | s, e, t, Debug str::p -> s, e, str::t, p
-  (*Return*)
-    (* Handling Return when exactly one value is on the stack, which should be preserved *)
-    | [x], Local ({ return_prog; _ }, parent_env), t, Return :: rest ->
-      ([x], parent_env, t, rest)  (* Return to caller's context with the rest of the program *)
-
-  (* Handling Return with no values on the stack; no value to return *)
-  | [], Local ({ return_prog; _ }, parent_env), t, Return :: rest ->
-      ([], parent_env, t, return_prog)  (* Continue with the caller's return program *)
-
-  (* Handling the Return from a function at the end of a program sequence without further commands *)
-  | s, Local ({ return_prog; _ }, parent_env), t, [] ->
-      (s, parent_env, t, return_prog)  (* Continue execution in parent environment *)
-
-  (* Error Handling for multiple values on the stack at Return: Error case *)
-  | _ :: _ :: _, Local ({ return_prog; _ }, _), t, Return :: _ ->
-      panic c "Return with multiple values on the stack is not allowed"
-
-  (* Attempt to return in a global context which is not permissible *)
-  | _, Global _, t, Return :: _ ->
-      panic c "Return command executed in global context"
-
-  (* Catch all for other unmatched cases *)
-  | _, _, _, _ -> panic c "Unhandled command or type error"
-  
-  
 let rec eval c =
   match c with
   | (_, Global _, t, []) -> t
@@ -410,6 +276,6 @@ let main () =
   | None -> print_endline "Parse Error"
   | Some t -> print_trace t
 
-let _ = main () 
+(* let _ = main () *)
 
 (* END OF FILE *)
